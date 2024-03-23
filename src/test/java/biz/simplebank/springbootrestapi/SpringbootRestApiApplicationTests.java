a34fbd20ca5decce197c02615fc89ebda6cc701e
Shahin Safaroff <tungsten.rust@mail.com>
2024-03-24 00:53:29 +0400

    first commit

---
+++ src/test/java/biz/simplebank/springbootrestapi/SpringbootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.simplebank.springbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
